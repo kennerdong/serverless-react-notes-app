4b4dc768f72571f4447d9bec1351c72261047550
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-23 00:31:12 +0800

    handled 404

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
-export default () =>
+
+export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
-  </Switch>;
+    {/* Finally, catch all unmatched routes */}
+    <Route component={NotFound} />
+  </Switch>
+);
