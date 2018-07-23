4ba41dd1c6928c00b28e43f3a62882e5fdf42c1d
xenon830 <14861146+kennerdong@users.noreply.github.com>
2018-07-24 01:23:11 +0800

    finished chapter: Add the Session to the State

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import AppliedRoute from "./components/AppliedRoute";
 
 
-export default () => (
+
+export default ({ childProps }) =>
   <Switch>
-    <Route path="/" exact component={Home} />
-    {/* Finally, catch all unmatched routes */}
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
-  </Switch>
-);
+  </Switch>;
+
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    // REGION: "ap-southeast-1",
+    BUCKET: "my-react-notes"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://d3f1crmguk.execute-api.ap-southeast-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_IBFdPh68L",
+    APP_CLIENT_ID: "1875cu5fv20adpdaot2ppindhr",
+    IDENTITY_POOL_ID: "ap-southeast-1:d1fddb5f-85de-44c3-817d-cbe1a0f767a1"
+  }
+};
